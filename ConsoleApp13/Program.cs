609cdc7d127818203d8b7b11912f44da51f48055
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-31 17:45:09 +0400

    finish

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student=new Student();
+
+            student.MethodGrades(new int[] { 1, 3, 4, 8, 11, 14 });
+
+            foreach (var item in student.Grades)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApp13/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Student
+    {
+        public int[] _grades;
+
+        public int[] Grades
+        {
+            get { return _grades; }
+        }
+
+        public void MethodGrades(int[] grades)
+        {
+            int count = 0;
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i]%2==0)
+                {
+                    count++;
+                }
+            }
+
+            _grades=new int[count];
+            count = 0;
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i]%2==0)
+                {
+                    _grades[count] = grades[i];
+                    count++;
+                }
+            }
+        }
+
+        
+    }
+}
